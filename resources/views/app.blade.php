10d0fb6b91dfd75b16f808e2eb1afe09cd1b9f0c
lieselot <lieselotslingerland@gmail.com>
2015-07-30 22:10:35 +0200

    basic css color & placing buttons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,40 +13,37 @@
 
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
+	
+	<header>
+		<h1>Verlanglijstjes</h1>
+		<a class="awsome" href="{{ url('/') }}"><i class="fa fa-gift"></i></a>
+	</header>
+	
+	<nav>		
+		<ul class="head-nav">
+			@if (Auth::guest())
+				<li class="btn"><a class="btn-round green btn-center" href="{{ url('/auth/login') }}"><i class="fa fa-sign-in"></i></a><span>Inloggen</span></li>
+			@else
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+					</ul>
+				</li>
+			@endif
+		</ul>
 	</nav>
-
+	
+	<section>
 	@yield('content')
+	</section>
+	
+	<footer>
+		<ul class="nav">
+			<li><a href="{{ url('/') }}"><i class="fa fa-gift"></i> Home</a></li>
+			<li><a href="#"><i class="fa fa-plus"></i> kado toevoegen</a></li>
+		</ul>
+	</footer>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 @extends('app')
 
+
 @section('content')
     <ul class="list-group">
+			<li class="list-group-item btn"><a class="btn-round red" href="#">red</a></li>
+			<li class="list-group-item btn"><a class="btn-round yellow" href="#">yellow</a></li>
+			<li class="list-group-item btn"><a class="btn-round blue" href="#">blue</a></li>
         @foreach($members as $member)
-            <li class="list-group-item"><a href="{{ url('/list/' . $member->name) }}">{{ $member->name }}</a></li>
-        @endforeach
+            <li class="list-group-item btn"><a class="btn-round green" href="{{ url('/list/' . $member->name) }}">{{ $member->name }}</a></li>
+        @endforeach		
     </ul>
 @endsection
